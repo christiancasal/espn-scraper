128aeb6cfd4fa851c7004d59095531d905a1ec39
ccasal <casal.christian@gmail.com>
2016-06-04 12:15:22 -0400

    init

---
+++ routes/headlines_controller.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+
+var Headlines = require('../models/headlines_model.js')[0];
+var db = require('../models/headlines_model.js')[1];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var sports = [
+    'nfl',
+    'nba',
+    'mlb',
+    'college-football',
+    'nhl'
+  ]
+
+  for (var i = 0; i < sports.length; i++) {
+    update_mongo_db(sports[i])
+  }
+
+  res.render('index', { title: 'Express' });
+});
+
+function update_mongo_db(league){
+  var queryUrl = 'https://espn.go.com'
+  request(queryUrl + "/" + league, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      console.log('request success!');
+
+      var $ = cheerio.load(html);
+
+      var article = new Headlines({
+        league: "",
+        title: "",
+        link: "",
+        text: "",
+        viewed: false,
+        note: ""
+      });
+
+
+      $('.headlines').each(function(i, element){
+
+        var pull = $(this).children().children().children()
+
+        for (var i = 0; i < pull.length; i++) {
+          article.league = league
+          article.title = pull[i].children[0].data;
+          article.link = pull[i].attribs.href;
+
+          //gets article text
+          request(queryUrl + article.link, function (error, response, html) {
+            if (!error && response.statusCode == 200) {
+              console.log('request success!');
+
+              var $ = cheerio.load(html);
+
+              $('.article-body').each(function(i, element){
+                article.text = $(this).children().text();
+                console.log(article);
+
+                //TODO check for existing articles
+
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+module.exports = router;
